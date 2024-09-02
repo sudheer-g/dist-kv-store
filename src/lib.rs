7982f953a24bdc104ae76ca73a32cc5c6d701191
sudheer-g <bharadwaj.sudheer@gmail.com>
2024-09-02 23:57:34 +0530

    Initial commit. Added basic structure and tests.

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+pub struct KvStore {
+}
+
+impl KvStore {
+    pub fn new() -> Self {
+        KvStore {}
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        panic!()
+    }
+
+    pub fn get(&mut self, key: String) -> Option<String> {
+        panic!()
+    }
+
+    pub fn remove(&mut self, key: String) {
+        panic!()
+    }
+}
