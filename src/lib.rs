e165de02e4c502c8f8a40d267a9ddddb2d86b1f5
sudheer-g <bharadwaj.sudheer@gmail.com>
2024-10-06 16:45:17 +0530

    Added basic in memory KV Store and tests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,4 @@
-pub struct KvStore {
-}
 
-impl KvStore {
-    pub fn new() -> Self {
-        KvStore {}
-    }
+pub use kv::KvStore;
 
-    pub fn set(&mut self, key: String, value: String) {
-        panic!()
-    }
-
-    pub fn get(&mut self, key: String) -> Option<String> {
-        panic!()
-    }
-
-    pub fn remove(&mut self, key: String) {
-        panic!()
-    }
-}
+mod kv;
